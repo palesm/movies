60acf6f859ec44b9d9480216d9dc59c39de7559e
palesmariann <mariann.pales@gmail.com>
2022-02-12 23:49:00 +0100

    fetch data from imdb

--- movies/src/api/api.js
+++ movies/src/api/api.js
@@ -3,6 +3,8 @@ import {resolve} from "./resolver";
 import {spaceToUnderscore} from "../helpers/helpers";
 
 const baseUrl = "https://en.wikipedia.org/w"
+const imdbUrl = "http://www.omdbapi.com"
+const apiKey = "1f7543a"
 
 export async function searchWiki(movie) {
   return await resolve(axios
@@ -13,5 +15,11 @@ export async function searchWiki(movie) {
 export async function getWikiContent(pageName) {
   return await resolve(axios
     .get(`${baseUrl}/api.php?action=query&prop=extracts&titles=${pageName}&exintro=&exsentences=5&explaintext=&redirects=&formatversion=2&format=json&origin=*`)
+    .then(res => res.data));
+}
+
+export async function searchImdb(movie) {
+  return await resolve(axios
+    .get(`${imdbUrl}/?t=${movie}&apikey=${apiKey}`)
     .then(res => res.data));
 }
--- movies/src/components/Movie.js
+++ movies/src/components/Movie.js
@@ -1,13 +1,12 @@
 import React, {useEffect, useState} from 'react';
+import * as api from "../api/api";
+import useToggleState from "../hooks/useToggleState";
+import {getPageName} from "../helpers/helpers";
 import {Chip, ListItemButton, ListItemSecondaryAction, Stack} from "@mui/material";
 import {Button, Collapse, Divider, Grid} from "@material-ui/core";
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import useToggleState from "../hooks/useToggleState";
+import {ExpandLess, ExpandMore}  from '@mui/icons-material';
 import {withStyles} from "@mui/styles";
 import styles from "../assets/styles/MovieStyles";
-import * as api from "../api/api";
-import {getPageName} from "../helpers/helpers";
 
 
 function Movie(props) {
@@ -15,31 +14,42 @@ function Movie(props) {
   const [clicked, toggleClick] = useToggleState(false);
   const [loading, setLoading] = useState(false);
   const [url, setUrl] = useState(null);
+  const [imdb, setImdb] = useState(null);
   const [description, setDescription] = useState("No description available");
 
   const handleClick = () => {
     //do not fetch again on close or if already fetched
-    if (!url && !clicked) {
-      setLoading(true)
-      //search page by name from wiki
-      api.searchWiki(props.name).then(res => {
-        // returns an array containing page url eg. "https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone"
-        if(res.data[3][0]) {
-          let url = res.data[3][0];
-          // extracting page name from url eg. "Harry_Potter_and_the_Philosopher%27s_Stone"
-          let pageName = getPageName(url);
-          setUrl(url);
-          // get short description by page name
-          api.getWikiContent(pageName).then(res => {
-            if (res.data && res.data.query.pages[0].extract) {
-              let description = res.data.query.pages[0].extract
-              setDescription(description)
-            } else setLoading(false)
-          })
-        } else setLoading(false)
-      })
-    }
+    if (!imdb  && !clicked) handleImdb()
+    if (!url && !clicked) handleWiki()
     toggleClick()
+  }
+  const handleWiki = () => {
+    setLoading(true)
+    //search page by name from wiki
+    api.searchWiki(props.name).then(res => {
+      // returns an array containing page url eg. "https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone"
+      if(res.data[3][0]) {
+        let url = res.data[3][0];
+        // extracting page name from url eg. "Harry_Potter_and_the_Philosopher%27s_Stone"
+        let pageName = getPageName(url);
+        setUrl(url);
+        // get short description by page name
+        api.getWikiContent(pageName).then(res => {
+          if (res.data && res.data.query.pages[0].extract) {
+            let description = res.data.query.pages[0].extract
+            setDescription(description)
+          } else setLoading(false)
+        })
+      } else setLoading(false)
+    })
+  }
+  const handleImdb = () => {
+    //search movie by name from imdb
+    api.searchImdb(props.name).then(res => {
+      if(res.data && res.data.imdbID) {
+        setImdb(`https://www.imdb.com/title/${res.data.imdbID}`)
+      }
+    })
   }
   useEffect(() => {
     return () => {
@@ -73,13 +83,15 @@ function Movie(props) {
           <>
             <Stack direction="row" spacing={2}>
               {url &&
-              <Button variant="contained" href={url} target="_blank">
-                Wikipedia
-              </Button>
+                <Button variant="contained" href={url} target="_blank">
+                  Wikipedia
+                </Button>
               }
-              <Button variant="contained">
-                IMDB
-              </Button>
+              {imdb &&
+                <Button variant="contained" href={imdb} target="_blank">
+                  IMDB
+                </Button>
+              }
               <Button variant="contained">
                 Related
               </Button>
